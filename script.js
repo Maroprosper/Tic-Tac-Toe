3edb7161e186673df6a5c41365c15f70e3e0756c
Ogbaudu Oghenemaro <maroprosper14@gmail.com>
2023-09-13 11:59:55 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+(function Gameboard(){
+    let gameboard = ['x','o','x','o','x','o'];
+})();
+function player(marker, name){
+    return {name, marker};
+}
+    
+(function displayGame(){
+    
+})();
+(function Game(){
+    let _user = player();
+    let _computer =
+    let _boxes = document.querySelectorAll('.box');
+    _boxes.forEach(item => item.addEventListener('click', displayMarker));
+    let displayMarker = () => {
+        
+    }
+})();
